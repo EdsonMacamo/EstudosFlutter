e4c432b6b07858afd165699bb404edfa3a51c1fb
Edson Macamo <edsonmacamo85@gmail.com>
2022-04-20 15:45:26 +0200

    adicionando boas praticas

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'app_widget.dart';
+
 main() {
-  runApp(AppWidget(
-    title: 'Edson 2',
-  ));
-}
-
-class AppWidget extends StatelessWidget {
-  final String title;
-
-  const AppWidget({
-    Key? key,
-    required this.title,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Text(
-          title,
-          textDirection: TextDirection.ltr,
-          style: TextStyle(color: Colors.white, fontSize: 50),
-        ),
-      ),
-    );
-  }
+  runApp(AppWidget());
 }
